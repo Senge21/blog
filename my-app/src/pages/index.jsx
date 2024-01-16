5a24f4e5886091e6d1ead068da92792b68efbed9
Dashka22 <dashka.nest1@gmail.com>
2024-01-16 10:01:04 +0800

    useState

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -1,9 +1,26 @@
-import styles from "@/styles/styles.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [number, setNumber] = useState(0);
+
+  const handleButton = (text) => {
+    setNumber(number + 1);
+    console.log("button darlaa" + " " + number);
+  };
+
+  const handleButtonTwo = () => {
+    setNumber(number - 1);
+    console.log("button darlaa");
+  };
+
   return (
-    <>
-      <div className={`${styles.box} ${styles.bgRed}`}>hahHAhahahahah</div>
-    </>
+    <div>
+      <h1>{number}</h1>
+      {/* Argument damjuulsan */}
+      <button onClick={() => handleButton("heelo")}>Nemeh</button>
+
+      {/* Argument damjuulaagui */}
+      <button onClick={handleButtonTwo}>Hasah</button>
+    </div>
   );
 }
--- my-app/src/pages/n/31u5.jsx
+++ my-app/src/pages/n/31u5.jsx
@@ -1,4 +1,5 @@
 const Ikon = () => {
+  
   return <div>Ikon page</div>;
 };
 
