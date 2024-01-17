6f7e4d752af1e309b4f04b4716971a0051f4bc0c
Dashka22 <dashka.nest1@gmail.com>
2024-01-17 17:02:18 +0800

    tailwindig huuchin projectteigo holboson

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -1,26 +1,3 @@
-import { useState } from "react";
-
 export default function Home() {
-  const [number, setNumber] = useState(0);
-
-  const handleButton = (text) => {
-    setNumber(number + 1);
-    console.log("button darlaa" + " " + number);
-  };
-
-  const handleButtonTwo = () => {
-    setNumber(number - 1);
-    console.log("button darlaa");
-  };
-
-  return (
-    <div>
-      <h1>{number}</h1>
-      {/* Argument damjuulsan */}
-      <button onClick={() => handleButton("heelo")}>Nemeh</button>
-
-      {/* Argument damjuulaagui */}
-      <button onClick={handleButtonTwo}>Hasah</button>
-    </div>
-  );
+  return <div className="bg-red-500 w-10 h-20">Hello</div>;
 }
